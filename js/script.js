9a2a011bf95b7cd54ae30fdb6f2a1745edc3655f
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-24 10:30:09 +0100

    [DONE] complite the exercise

--- js/script.js
+++ js/script.js
@@ -53,3 +53,8 @@ function isSumEven(number1, number2) {
     return isEven;
 }
 
+if ((isSumEven(userNumber, computerNumber) && userChoice === 'pari') || (!isSumEven(userNumber, computerNumber) && userChoice === 'dispari')) {
+    console.log('Hai vinto');
+} else {
+    console.log('Hai perso');
+}

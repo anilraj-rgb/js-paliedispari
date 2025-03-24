433177bf41b8c1f024c06b409fb4411c9d7f3674
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-24 10:24:59 +0100

    [UPD] firste exercise complited

--- js/script.js
+++ js/script.js
@@ -1,34 +1,22 @@
 // Palindroma
-// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
 
-// Definisco la funzione che mi controlla se la stringa è palindroma
-// La funzione prende in input la parola inserita dell'utente
-// Divide la parola in lettere 
-// inverto la posizione delle lettere
-// Riunisco le lettere e salvo il tutto in una nuova variabile
-// confronto la parola data daall'utente con quella invertita
-// se sono uguali allora la parola è palindroma, altrimenti no
+// Chiedo all'utente di inserire una parola
+let word = prompt('Inserisci una parola');
 
-//permetto all'utente di inserire la parola
-// chiamo la funzione passandogli la parola 
-// ?????
+// Funzione per capire se la parola inserita è palindroma
 
-//DEFINIZIONE/DICHIARAZIONE DI UNA FUNZIONE: il comportamneto,quello che deve fare la funzione
-function checkpalondrima(strng){
-    // dichiaro la variabile che coterrà la parola invertita. Uso split, reverse e join per trovarla ed assegnarla
-    // let splitted = string.split('');
-    // let reversed = splitted.reverse('');
-    // let joined = reversed.join('');
-    // console.log(splitted);
-    // console.log(reversed)
-    // console.log(joined)
-    let check = String.split('').reverse().join('');
+function isPalindrome(string) {
+    let lowerCaseWord = string.toLowerCase();
+    let reverseWord = lowerCaseWord.split('').reverse().join('');
+    if (lowerCaseWord === reverseWord) {
+        console.log('La parola inserita è palindroma');
+    } else {
+        console.log('La parola inserita non è palindroma');
+    }
 }
 
-// permetto all'utente di inserire la parola
-let word = prompt('inserisci la parola');
+isPalindrome(word);
 
-// chiamata di funzione: eseguo la funzione
-checkpalindroma(word);
-
-
+// FINE ESERCIZIO 1

23e90f0f98202d82deea9268d0e59e2a728cba8c
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-24 10:15:23 +0100

    [UPD] create the structure

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// Definisco la funzione che mi controlla se la stringa è palindroma
+// La funzione prende in input la parola inserita dell'utente
+// Divide la parola in lettere 
+// inverto la posizione delle lettere
+// Riunisco le lettere e salvo il tutto in una nuova variabile
+// confronto la parola data daall'utente con quella invertita
+// se sono uguali allora la parola è palindroma, altrimenti no
+
+//permetto all'utente di inserire la parola
+// chiamo la funzione passandogli la parola 
+// ?????
+
+//DEFINIZIONE/DICHIARAZIONE DI UNA FUNZIONE: il comportamneto,quello che deve fare la funzione
+function checkpalindrima(strng){
+    // dichiaro la variabile che coterrà la parola invertita. Uso split, reverse e join per trovarla ed assegnarla
+    // let splitted = string.split('');
+    // let reversed = splitted.reverse('');
+    // let joined = reversed.join('');
+    // console.log(splitted);
+    // console.log(reversed)
+    // console.log(joined)
+    let check = String.split('').reverse().join('');
+}
+
+// permetto all'utente di inserire la parola
